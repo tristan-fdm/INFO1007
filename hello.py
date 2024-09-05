cdf516b223eee5bdd414ba9363f38ca84172c408
Tristan Fabre de Munck <tristanfabredemunck@Tristans-MacBook-Air.local>
2024-09-04 20:11:50 -0400

    les questions sont répondues

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+print(math.pi*rayon**2)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Fabre de Munck Tristan")
